c1fadc41cfa1ab4c0f39a207a7dcfd3786a51177
unknown <ashrafulislamprodev@gmail.com>
2023-07-17 10:32:29 +0600

    first push for crypto

---
+++ assets/main.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+
+    $('.hero-des-wrapper').fuwatto({
+        duration: 2000,
+        slide:'left-right',
+        distance: 600,
+        adjustment: {
+            top: 0,
+            left: 0,
+          },
+
+      });
+      $('.security-cards').fuwatto({
+        duration: 2000,
+        slide:'right-left',
+        distance: 600,
+        adjustment: {
+            top: 0,
+            left: 0,
+          },
+      });
+      $('.analysis-img').fuwatto({ 
+        duration: 2000,
+        slide:'left-right',
+        distance: 600,
+        adjustment: {
+            top: 0,
+            left: 0,
+          },
+      });
+      $('.analysis-des').fuwatto({ 
+        duration: 2000,
+        slide:'right-left',
+        distance: 600,
+        adjustment: {
+            top: 0,
+            left: 0,
+          },
+      });
+      $('.benifit-cards').fuwatto({ 
+        duration: 2000,
+        slide:'bottom-top',
+        distance: 600,
+        adjustment: {
+            top: 0,
+            left: 0,
+          },
+      });
+    //   testimonial
+      $('.t-left').fuwatto({ 
+        duration: 2000,
+        slide:'left-right',
+        distance: 600,
+        adjustment: {
+            top: 0,
+            left: 0,
+          },
+      });
+      $('.t-right').fuwatto({ 
+        duration: 2000,
+        slide:'right-left',
+        distance: 600,
+      });
+    //   latest news
+    $('.ln-left').fuwatto({ 
+        duration: 2000,
+        slide:'left-right',
+        distance: 600,
+        adjustment: {
+            top: 0,
+            left: 0,
+          },
+      });
+      $('.ln-right').fuwatto({ 
+        duration: 2000,
+        slide:'right-left',
+        distance: 600,
+        adjustment: {
+            top: 0,
+            left: 0,
+          },
+      });
+  });
